2ac774fbbb99fcaf2f4ec9195edcbadcc9272693
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-03-30 13:26:19 +0100

    topbar styles

---
+++ components/shared/Topbar.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { ArrowRightEndOnRectangleIcon, ArrowRightStartOnRectangleIcon } from "@heroicons/react/24/solid";
+import { SignedIn, SignedOut, SignOutButton } from "@clerk/nextjs";
+
+function Topbar() {
+	return (
+		<nav className='fixed w-full top-0 flex justify-between px-3 py-3 md:px-14 lg:px-32 border-b border-light-2'>
+			<Link href='/' className='flex items-center gap-4'>
+				<Image
+					src='/assets/piggy-bank.png'
+					width={30}
+					height={30}
+					alt='piggy bank pink'
+				/>
+				<span className='font-bold text-lg hidden sm:block'>PiggyBank</span>
+			</Link>
+			<SignedOut>
+				<Link
+					href='/sign-in'
+					className='main-btn-circle'>
+					<ArrowRightEndOnRectangleIcon className='h-6 w-6' />
+				</Link>
+				<Link href="/sign-in" className="main-btn hidden md:block">
+					<span>Sign In</span>
+				</Link>
+			</SignedOut>
+			<SignedIn>
+                <SignOutButton>
+                    <div
+                        className='main-btn-circle'>
+                        <ArrowRightStartOnRectangleIcon className='h-6 w-6' />
+                    </div>
+                </SignOutButton>
+				<SignOutButton>
+					<Link href="/sign-in" className="main-btn hidden md:block">
+						<span>Sign Out</span>
+					</Link>
+				</SignOutButton>
+			</SignedIn>
+		</nav>
+	);
+}
+
+export default Topbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,21 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#087E8B",
+				"primary-2": "#0baabc",
+				"light-1": "#fff",
+				"light-2": "#e6e6e6",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
