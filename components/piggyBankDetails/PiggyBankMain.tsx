7a5c57cfcbc16de0e6f5037ef9cb0ea17c3acbb5
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-04-08 12:21:33 +0200

    selecting btn when view is visited

--- components/piggyBankDetails/PiggyBankMain.tsx
+++ components/piggyBankDetails/PiggyBankMain.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { PIGGY_BANK_ADD_PERSON, PIGGY_BANK_EDIT, PIGGY_BANK_PAYMENTS, PIGGY_BANK_PEOPLE } from '@/constants';
+import { PIGGY_BANK_ADD_PERSON, PIGGY_BANK_BTNS, PIGGY_BANK_EDIT, PIGGY_BANK_PAYMENTS, PIGGY_BANK_PEOPLE } from '@/constants';
 import React, { useState } from 'react'
 import PiggyBankPeople from './PiggyBankPeople';
 import PiggyBankAddPerson from './PiggyBankAddPerson';
@@ -14,14 +14,21 @@ type Props = {
 
 function PiggyBankMain({ piggyBank, currentUserId }: Props) {
     const [view, setView] = useState(PIGGY_BANK_PEOPLE);
+    const [btnIndex, setBtnIndex] = useState(0)
+
+    function handleSelectBtn(index: number, viewText: string) {
+      setBtnIndex(index)
+      setView(viewText)
+    }
 
   return (
     <div className='flex flex-col gap-5 mt-5'>
         <div className='flex justify-center flex-wrap gap-x-7 gap-y-5'>
-            <button className='secondary-btn' onClick={() => setView(PIGGY_BANK_PEOPLE)}>Osoby</button>
-            <button className='secondary-btn' onClick={() => setView(PIGGY_BANK_ADD_PERSON)}>Dodaj osobę</button>
-            <button className='secondary-btn' onClick={() => setView(PIGGY_BANK_PAYMENTS)}>Wpłaty</button>
-            <button className='secondary-btn' onClick={() => setView(PIGGY_BANK_EDIT)}>Edytuj</button>
+            {PIGGY_BANK_BTNS.map((btn, index) => {
+              return (
+                <button key={index} className={`secondary-btn ${index == btnIndex && "bg-secondary-1"}`} onClick={() => handleSelectBtn(index, btn.viewText)}>{btn.btnText}</button>
+              )
+            })}
         </div>
         {view === PIGGY_BANK_PEOPLE && <PiggyBankPeople />}
         {view === PIGGY_BANK_ADD_PERSON && <PiggyBankAddPerson userId={currentUserId} piggyBankId={piggyBank?._id} />}
--- constants/index.ts
+++ constants/index.ts
@@ -2,3 +2,10 @@ export const PIGGY_BANK_PEOPLE = "PIGGY_BANK_PEOPLE";
 export const PIGGY_BANK_ADD_PERSON = "PIGGY_BANK_ADD_PERSON";
 export const PIGGY_BANK_PAYMENTS = "PIGGY_BANK_PAYMENTS";
 export const PIGGY_BANK_EDIT = "PIGGY_BANK_EDIT";
+
+export const PIGGY_BANK_BTNS = [
+    { btnText: "Osoby", viewText: PIGGY_BANK_PEOPLE },
+    { btnText: "Dodaj osobę", viewText: PIGGY_BANK_ADD_PERSON },
+    { btnText: "Wpłaty", viewText: PIGGY_BANK_PAYMENTS },
+    { btnText: "Edytuj", viewText: PIGGY_BANK_EDIT },
+]
