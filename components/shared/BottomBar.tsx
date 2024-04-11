4e0f49d9833e57385145b145e748ff6a9923b53d
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-04-11 19:27:39 +0200

    displaying active routes in side bar and bottom bar

--- components/shared/BottomBar.tsx
+++ components/shared/BottomBar.tsx
@@ -1,16 +1,23 @@
+"use client"
+
 import Link from "next/link";
 import React from "react";
 import { sidebarItems } from "./LeftSidebar";
+import { usePathname } from "next/navigation";
 
 function BottomBar() {
+	const pathName = usePathname();
+
 	return (
 		<nav className='fixed bottom-0 w-full flex justify-between py-3 px-8 bg-light-1 border-t border-t-light-2 md:hidden'>
 			{sidebarItems.map(item => {
+				const isActiveRoute = item.route === pathName;
+
 				return (
 					<Link
 						href={item.route}
 						key={item.route}
-						className='flex flex-col gap-1 items-center'>
+						className={`flex flex-col gap-1 items-center ${isActiveRoute && "text-primary font-semibold"}`}>
 						{item.icon}
 						<span className='text-xs hidden sm:block'>{item.text}</span>
 					</Link>
--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import {
 	HomeIcon,
@@ -6,6 +8,7 @@ import {
 	MagnifyingGlassIcon,
 } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const sidebarItems = [
 	{ route: "/", icon: <HomeIcon className='h-6 w-6' />, text: "Główny" },
@@ -23,15 +26,19 @@ export const sidebarItems = [
 ];
 
 function LeftSidebar() {
+	const pathName = usePathname();
+
 	return (
 		<aside className='sticky top-0 left-0 h-screen w-fit pt-24 px-6 lg:px-8 border-r border-r-light-2 bg-light-1 hidden md:block'>
 			<div className='flex flex-col gap-7'>
 				{sidebarItems.map(item => {
+					const isRouteActive = item.route === pathName;
+
 					return (
 						<Link
 							href={item.route}
 							key={item.route}
-							className='flex items-center gap-3 lg:w-[200px] p-2 rounded-lg text-xl hover:bg-light-2 transition'>
+							className={`flex items-center gap-3 lg:w-[200px] p-2 rounded-lg text-xl hover:bg-light-2 transition ${isRouteActive && "bg-light-2 font-semibold"}`}>
 							{item.icon}
 							<span className='hidden lg:block'>{item.text}</span>
 						</Link>
